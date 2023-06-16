7dcd7534dafb12260f99bba8c1e6bcb8efd40c03
Arghya <arghyadutta080@gmail.com>
2023-06-16 13:14:25 +0530

    Styling of newly added component in Home section UI

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -21,11 +21,11 @@ function Home() {
     <div className='container mb-4'>
       <div className='mb-4 d-flex flex-column align-items-center justify-content-center' style={{ width: "100%" }}>
         <div className='text-center display-4 mt-2' style={{ fontFamily: "Lora", minWidth: "380px", maxWidth: "700px" }}> <em>Empowering tomorrow's leaders, today</em></div>
-        <div className='d-flex justify-content-center fs-3 my-4 py-2' style={{ fontFamily: "Lora" }}>
+        <div className='d-flex justify-content-center align-items-center fs-3 my-4 py-2 container flex-wrap' style={{ fontFamily: "Lora" }}>
           <em style={{ marginRight: "17px" }}>Rotaract Year</em>
           <div className="dropdown ">
-            <button className="btn dropdown-toggle fs-5" style={{ backgroundColor: "#D0B9EC" }} type="button" data-bs-toggle="dropdown" aria-expanded="false">
-              2022 - 2023
+            <button className="btn dropdown-toggle fs-5 rounded-5 py-1 px-3" style={{ backgroundColor: "#D0B9EC" }} type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <em className='semi-bold'>2022 - 2023</em>
             </button>
             <ul className="dropdown-menu ">
               <li><div className="dropdown-item my-2">2022 - 2023</div></li>
@@ -37,7 +37,7 @@ function Home() {
             </ul>
           </div>
         </div>
-        <button onClick={join_notification} className="btn my-1" type="submit" style={{ color: "white", backgroundColor: "#B4637A" }}><Link className="nav-link fw-semibold fs-6" to="/">Join Us</Link></button>
+        <button onClick={join_notification} className="btn rounded-3 my-1" type="submit" style={{ color: "white", backgroundColor: "#B4637A" }}><Link className="nav-link fw-semibold fs-6" to="/">Join Us</Link></button>
       </div>
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mt-4 mb-5 pb-4">
         <div className="col mb-6 px-3 d-flex flex-column justify-content-center align-items-center">
